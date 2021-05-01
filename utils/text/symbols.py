a95810b027b4bf79af6c80824be778be4d5d0846
cschaefer26 <c.schaefer.home@gmail.com>
2021-05-01 20:13:58 +0200

    Extra phons.

--- utils/text/symbols.py
+++ utils/text/symbols.py
@@ -10,7 +10,20 @@ _pad = '_'
 _punctuation = '!\'(),.:;? '
 _special = '-'
 
-_phonemes = ['a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ç', 'ð', 'ø', 'ŋ', 'œ', 'ɐ', 'ɑ', 'ɔ', 'ə', 'ɛ', 'ɝ', 'ɡ', 'ɪ', 'ʁ', 'ʃ', 'ʊ', 'ʌ', 'ʏ', 'ʒ', 'ʔ', 'ː', '̃', '̍', '̩', 'θ']
+# Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
+_arpabet = ['@' + s for s in cmudict.valid_symbols]
 
-phonemes = list(_pad + _punctuation + _special) + _phonemes
+# Phonemes
+_vowels = 'iyɨʉɯuɪʏʊeøɘəɵɤoɛœɜɞʌɔæɐaɶɑɒᵻ'
+_non_pulmonic_consonants = 'ʘɓǀɗǃʄǂɠǁʛ'
+_pulmonic_consonants = 'pbtdʈɖcɟkɡqɢʔɴŋɲɳnɱmʙrʀⱱɾɽɸβfvθðszʃʒʂʐçʝxɣχʁħʕhɦɬɮʋɹɻjɰlɭʎʟ'
+_suprasegmentals = 'ˈˌːˑ'
+_other_symbols = 'ʍwɥʜʢʡɕʑɺɧ'
+_diacrilics = 'ɚ˞ɫ'
+_extra_phons = ['g', 'ɝ', '̃', '̍', '̥', '̩', '̯', '͡'] # from wiktionary
+
+phonemes = list(
+   _pad + _punctuation + _special + _vowels + _non_pulmonic_consonants
+   + _pulmonic_consonants + _suprasegmentals + _other_symbols + _diacrilics) + _extra_phons
+
 phonemes_set = set(phonemes)
